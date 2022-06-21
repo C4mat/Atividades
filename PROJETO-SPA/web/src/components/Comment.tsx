fe1d1d7298ee85df917ccd955e07b4ccaad7a327
Matheus Claudino Ribeiro <74937108+C4mat@users.noreply.github.com>
2022-06-20 22:24:37 -0300

    Add files via upload

--- PROJETO-SPA/web/src/components/Comment.tsx
+++ PROJETO-SPA/web/src/components/Comment.tsx
@@ -2,7 +2,7 @@ export const Comment = () => {
     return (
 
         <div className="bg-zinc-500 p-8 shadow-sm rounded-xl mx-10 ">
-
+            <iframe className=" aspect-video place-items-center w-1/2 container mx-auto px-4  space-y-4  " src="https://www.youtube.com/embed/j5FZTcqkqT4?controls=1oop=1&amp;///start=2" ></iframe>
             <div className="pt-6  text-center space-y-4">
                 <div className="text-lg font-bold">
                     Parabéns você concluiu o curso !!!!
